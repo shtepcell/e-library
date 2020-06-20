bddaf2d66d076c1d4f982e5ff34ec8f7267df038
Vladislav Telichko <vladislav.telichko@ya.ru>
2020-06-20 23:36:47 +0300

    Start Navigation

---
+++ src/features/Navigation/Navigation.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+import './Navigation.scss';
+
+export class Navigation extends PureComponent {
+    render() {
+        return (
+            <div className="Navigation">
+
+            </div>
+        );
+    }
+}
